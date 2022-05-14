9f505cbc848d5cd38c558eb1c3f779e47a798153
AadhavanPL <aadhavanlenin@gmail.com>
2022-05-14 15:52:06 +0300

    added all the buttons

---
+++ src/App.js
@@ -0,0 +1,319 @@
+//import logo from './logo.svg';
+//import ReactDOM  from 'react-dom';
+import * as React from "react";
+import Button from '@mui/material/Button'
+import './App.css';
+
+
+function App() {
+  return (
+    <div>
+        <div className='App'>
+          <Button>‎</Button>
+          <Button variant = "contained">அ</Button>
+          <Button variant = "contained">ஆ</Button>
+          <Button variant = "contained">இ</Button>
+          <Button variant = "contained">ஈ</Button>
+          <Button variant = "contained">உ</Button>
+          <Button variant = "contained">ஊ</Button>
+          <Button variant = "contained">எ</Button>
+          <Button variant = "contained">ஏ</Button>
+          <Button variant = "contained">ஐ</Button>
+          <Button variant = "contained">ஒ</Button>
+          <Button variant = "contained">ஓ</Button>
+          <Button variant = "contained">ஔ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">க்</Button>
+          <Button variant = "contained">க</Button>
+          <Button variant = "contained">கா</Button>
+          <Button variant = "contained">கி</Button>
+          <Button variant = "contained">கீ</Button>
+          <Button variant = "contained">கு</Button>
+          <Button variant = "contained">கூ</Button>
+          <Button variant = "contained">கெ</Button>
+          <Button variant = "contained">கே</Button>
+          <Button variant = "contained">கை</Button>
+          <Button variant = "contained">கொ</Button>
+          <Button variant = "contained">கோ</Button>
+          <Button variant = "contained">கௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ங்</Button>
+          <Button variant = "contained">ங</Button>
+          <Button variant = "contained">ஙா</Button>
+          <Button variant = "contained">ஙி</Button>
+          <Button variant = "contained">ஙீ</Button>
+          <Button variant = "contained">ஙு</Button>
+          <Button variant = "contained">ஙூ</Button>
+          <Button variant = "contained">ஙெ</Button>
+          <Button variant = "contained">ஙே</Button>
+          <Button variant = "contained">ஙை</Button>
+          <Button variant = "contained">ஙொ</Button>
+          <Button variant = "contained">ஙோ</Button>
+          <Button variant = "contained">ஙௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ச்</Button>
+          <Button variant = "contained">ச</Button>
+          <Button variant = "contained">சா</Button>
+          <Button variant = "contained">சி</Button>
+          <Button variant = "contained">சீ</Button>
+          <Button variant = "contained">சு</Button>
+          <Button variant = "contained">சூ</Button>
+          <Button variant = "contained">செ</Button>
+          <Button variant = "contained">சே</Button>
+          <Button variant = "contained">சை</Button>
+          <Button variant = "contained">சொ</Button>
+          <Button variant = "contained">சோ</Button>
+          <Button variant = "contained">சௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ஞ்</Button>
+          <Button variant = "contained">ஞ</Button>
+          <Button variant = "contained">ஞா</Button>
+          <Button variant = "contained">ஞி</Button>
+          <Button variant = "contained">ஞீ</Button>
+          <Button variant = "contained">ஞு</Button>
+          <Button variant = "contained">ஞூ</Button>
+          <Button variant = "contained">ஞெ</Button>
+          <Button variant = "contained">ஞே</Button>
+          <Button variant = "contained">ஞை</Button>
+          <Button variant = "contained">ஞொ</Button>
+          <Button variant = "contained">ஞோ</Button>
+          <Button variant = "contained">ஞௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ட்</Button>
+          <Button variant = "contained">ட</Button>
+          <Button variant = "contained">டா</Button>
+          <Button variant = "contained">டி</Button>
+          <Button variant = "contained">டீ</Button>
+          <Button variant = "contained">டு</Button>
+          <Button variant = "contained">டூ</Button>
+          <Button variant = "contained">டெ</Button>
+          <Button variant = "contained">டே</Button>
+          <Button variant = "contained">டை</Button>
+          <Button variant = "contained">டொ</Button>
+          <Button variant = "contained">டோ</Button>
+          <Button variant = "contained">டௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ண்</Button>
+          <Button variant = "contained">ண</Button>
+          <Button variant = "contained">ணா</Button>
+          <Button variant = "contained">ணி</Button>
+          <Button variant = "contained">ணீ</Button>
+          <Button variant = "contained">ணு</Button>
+          <Button variant = "contained">ணூ</Button>
+          <Button variant = "contained">ணெ</Button>
+          <Button variant = "contained">ணே</Button>
+          <Button variant = "contained">ணை</Button>
+          <Button variant = "contained">ணொ</Button>
+          <Button variant = "contained">ணோ</Button>
+          <Button variant = "contained">ணௌ</Button>
+        </div>
+        
+        <div className='App'>
+          <Button variant = "contained">த்</Button>
+          <Button variant = "contained">த</Button>
+          <Button variant = "contained">தா</Button>
+          <Button variant = "contained">தி</Button>
+          <Button variant = "contained">தீ</Button>
+          <Button variant = "contained">து</Button>
+          <Button variant = "contained">தூ</Button>
+          <Button variant = "contained">தெ</Button>
+          <Button variant = "contained">தே</Button>
+          <Button variant = "contained">தை</Button>
+          <Button variant = "contained">தொ</Button>
+          <Button variant = "contained">தோ</Button>
+          <Button variant = "contained">தௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ந்</Button>
+          <Button variant = "contained">ந</Button>
+          <Button variant = "contained">நா</Button>
+          <Button variant = "contained">நி</Button>
+          <Button variant = "contained">நீ</Button>
+          <Button variant = "contained">நு</Button>
+          <Button variant = "contained">நூ</Button>
+          <Button variant = "contained">நெ</Button>
+          <Button variant = "contained">நே</Button>
+          <Button variant = "contained">நை</Button>
+          <Button variant = "contained">நொ</Button>
+          <Button variant = "contained">நோ</Button>
+          <Button variant = "contained">நௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ப்</Button>
+          <Button variant = "contained">ப</Button>
+          <Button variant = "contained">பா</Button>
+          <Button variant = "contained">பி</Button>
+          <Button variant = "contained">பீ</Button>
+          <Button variant = "contained">பு</Button>
+          <Button variant = "contained">பூ</Button>
+          <Button variant = "contained">பெ</Button>
+          <Button variant = "contained">பே</Button>
+          <Button variant = "contained">பை</Button>
+          <Button variant = "contained">பொ</Button>
+          <Button variant = "contained">போ</Button>
+          <Button variant = "contained">பௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ம்</Button>
+          <Button variant = "contained">ம</Button>
+          <Button variant = "contained">மா</Button>
+          <Button variant = "contained">மி</Button>
+          <Button variant = "contained">மீ</Button>
+          <Button variant = "contained">மு</Button>
+          <Button variant = "contained">மூ</Button>
+          <Button variant = "contained">மெ</Button>
+          <Button variant = "contained">மே</Button>
+          <Button variant = "contained">மை</Button>
+          <Button variant = "contained">மொ</Button>
+          <Button variant = "contained">மோ</Button>
+          <Button variant = "contained">மௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ய்</Button>
+          <Button variant = "contained">ய</Button>
+          <Button variant = "contained">யா</Button>
+          <Button variant = "contained">யி</Button>
+          <Button variant = "contained">யீ</Button>
+          <Button variant = "contained">யு</Button>
+          <Button variant = "contained">யூ</Button>
+          <Button variant = "contained">யெ</Button>
+          <Button variant = "contained">யே</Button>
+          <Button variant = "contained">யை</Button>
+          <Button variant = "contained">யொ</Button>
+          <Button variant = "contained">யோ</Button>
+          <Button variant = "contained">யௌ</Button>
+        </div>
+
+
+        <div className='App'>
+          <Button variant = "contained">ர்</Button>
+          <Button variant = "contained">ர</Button>
+          <Button variant = "contained">ரா</Button>
+          <Button variant = "contained">ரி</Button>
+          <Button variant = "contained">ரீ</Button>
+          <Button variant = "contained">ரு</Button>
+          <Button variant = "contained">ரூ</Button>
+          <Button variant = "contained">ரெ</Button>
+          <Button variant = "contained">ரே</Button>
+          <Button variant = "contained">ரை</Button>
+          <Button variant = "contained">ரொ</Button>
+          <Button variant = "contained">ரோ</Button>
+          <Button variant = "contained">ரௌ</Button>
+        </div>
+ 
+        <div className='App'>
+          <Button variant = "contained">ல்</Button>
+          <Button variant = "contained">ல</Button>
+          <Button variant = "contained">லா</Button>
+          <Button variant = "contained">லி</Button>
+          <Button variant = "contained">லீ</Button>
+          <Button variant = "contained">லு</Button>
+          <Button variant = "contained">லூ</Button>
+          <Button variant = "contained">லெ</Button>
+          <Button variant = "contained">லே</Button>
+          <Button variant = "contained">லை</Button>
+          <Button variant = "contained">லொ</Button>
+          <Button variant = "contained">லோ</Button>
+          <Button variant = "contained">லௌ</Button>
+        </div> 
+
+        <div className='App'>
+          <Button variant = "contained">வ்</Button>
+          <Button variant = "contained">வ</Button>
+          <Button variant = "contained">வா</Button>
+          <Button variant = "contained">வி</Button>
+          <Button variant = "contained">வீ</Button>
+          <Button variant = "contained">வு</Button>
+          <Button variant = "contained">வூ</Button>
+          <Button variant = "contained">வெ</Button>
+          <Button variant = "contained">வே</Button>
+          <Button variant = "contained">வை</Button>
+          <Button variant = "contained">வொ</Button>
+          <Button variant = "contained">வோ</Button>
+          <Button variant = "contained">வௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ழ்</Button>
+          <Button variant = "contained">ழ</Button>
+          <Button variant = "contained">ழா</Button>
+          <Button variant = "contained">ழி</Button>
+          <Button variant = "contained">ழீ</Button>
+          <Button variant = "contained">ழு</Button>
+          <Button variant = "contained">ழூ</Button>
+          <Button variant = "contained">ழெ</Button>
+          <Button variant = "contained">ழே</Button>
+          <Button variant = "contained">ழை</Button>
+          <Button variant = "contained">ழொ</Button>
+          <Button variant = "contained">ழோ</Button>
+          <Button variant = "contained">ழௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ள்	</Button>
+          <Button variant = "contained">ள</Button>
+          <Button variant = "contained">ளா</Button>
+          <Button variant = "contained">ளி</Button>
+          <Button variant = "contained">ளீ</Button>
+          <Button variant = "contained">ளு</Button>
+          <Button variant = "contained">ளூ</Button>
+          <Button variant = "contained">ளெ</Button>
+          <Button variant = "contained">ளே</Button>
+          <Button variant = "contained">ளை</Button>
+          <Button variant = "contained">ளொ</Button>
+          <Button variant = "contained">ளோ</Button>
+          <Button variant = "contained">ளௌ</Button>
+        </div>
+
+        <div className='App'>
+          <Button variant = "contained">ற்</Button>
+          <Button variant = "contained">ற</Button>
+          <Button variant = "contained">றா</Button>
+          <Button variant = "contained">றி</Button>
+          <Button variant = "contained">றீ</Button>
+          <Button variant = "contained">று</Button>
+          <Button variant = "contained">றூ</Button>
+          <Button variant = "contained">றெ</Button>
+          <Button variant = "contained">றே</Button>
+          <Button variant = "contained">றை</Button>
+          <Button variant = "contained">றொ</Button>
+          <Button variant = "contained">றோ</Button>
+          <Button variant = "contained">றௌ</Button>
+        </div>
+        
+        <div className='App'>
+          <Button variant = "contained">ன்</Button>
+          <Button variant = "contained">ன</Button>
+          <Button variant = "contained">னா</Button>
+          <Button variant = "contained">னி</Button>
+          <Button variant = "contained">னீ</Button>
+          <Button variant = "contained">னு</Button>
+          <Button variant = "contained">னூ</Button>
+          <Button variant = "contained">னெ</Button>
+          <Button variant = "contained">னே</Button>
+          <Button variant = "contained">னை</Button>
+          <Button variant = "contained">னொ</Button>
+          <Button variant = "contained">னோ</Button>
+          <Button variant = "contained">னௌ</Button>
+        </div>
+    </div>
+  );
+}
+
+export default App;
